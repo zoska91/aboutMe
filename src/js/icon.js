774c6679cb7d0722bfe819bf015187174c5bc482
zoska91 <zoska91@wp.pl>
2019-05-11 18:35:38 +0200

    add footer - finish. add projects - not finish (add photos, icons, only phone. to do: filters, gits, RWD)

---
+++ src/js/icon.js
@@ -0,0 +1,9 @@
+const iconsFilter = [...document.querySelectorAll(".icon-filter")];
+const iconsUsed = [...document.querySelectorAll(".used-icon")];
+const icons = iconsFilter.concat(iconsUsed);
+
+icons.forEach(icon => {
+  let name = icon.getAttribute("data-name");
+  name = name + ".png";
+  icon.style.backgroundImage = `url('./../../src/images/${name}')`;
+});
