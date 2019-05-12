ff2b11658dedadf0c8631e0bb35dcc0f00eec2a6
zoska91 <zoska91@wp.pl>
2019-05-12 10:05:28 +0200

    add rwd in projects

--- src/js/icon.js
+++ src/js/icon.js
@@ -5,5 +5,5 @@ const icons = iconsFilter.concat(iconsUsed);
 icons.forEach(icon => {
   let name = icon.getAttribute("data-name");
   name = name + ".png";
-  icon.style.backgroundImage = `url('./../../src/images/${name}')`;
+  icon.style.backgroundImage = `url('./../src/images/${name}')`;
 });
