bae8a43e922a676c3e584708c5aa766dddb187ea
zoska91 <zoska91@wp.pl>
2019-05-14 09:33:15 +0200

    begin filters. add one project

--- src/js/icon.js
+++ src/js/icon.js
@@ -7,3 +7,44 @@ icons.forEach(icon => {
   name = name + ".png";
   icon.style.backgroundImage = `url('./../src/images/${name}')`;
 });
+
+// filters
+
+const filters = [...document.querySelectorAll(".icon-filter")];
+let activeFilters = [];
+const projects = [...document.querySelectorAll(".project")];
+
+const checkProjects = () => {
+  projects.forEach(project => {
+    let used = project.getAttribute("data-filters");
+    const projectsFilters = used.split(" ");
+
+    // forEach - czy kazdy activeFilter jest w projectsFilters,
+
+    for (let i = 0; i <= activeFilters.length; i++) {
+      let name = activeFilters[i];
+      name = name.substring(0, name.indexOf("-"));
+      console.log(name);
+      const index = projectsFilters.indexOf(name);
+      console.log(index);
+    }
+  });
+};
+
+const addFilter = name => {
+  if (activeFilters.indexOf(name) === -1) {
+    activeFilters.push(name);
+  } else {
+    const index = activeFilters.indexOf(name);
+    activeFilters.splice(index, 1);
+  }
+  checkProjects();
+};
+
+filters.forEach(icon => {
+  icon.addEventListener("click", () => {
+    icon.classList.toggle("on");
+    const name = icon.getAttribute("data-name");
+    addFilter(name);
+  });
+});
