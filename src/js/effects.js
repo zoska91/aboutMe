376033077f35df3e4c39cb4a41d1413413b4beac
zoska91 <zoska91@wp.pl>
2019-08-21 12:08:09 +0200

    add new project

--- src/js/effects.js
+++ src/js/effects.js
@@ -1,52 +1,52 @@
 const start = setTimeout(() => {
-  $(".icons").addClass("on-effect");
-  $(".links").addClass("on-effect");
-  $(".header-button").addClass("on-effect");
+  $('.icons').addClass('on-effect');
+  $('.links').addClass('on-effect');
+  $('.header-button').addClass('on-effect');
 }, 200);
 
-const articleFromTop = $("article").offset().top;
+const articleFromTop = $('article').offset().top;
 const WindowHeight = $(window).height();
-const photoFromTop = $(".photo").offset().top;
-const filtersFromTop = $(".filters").offset().top;
+const photoFromTop = $('.photo').offset().top;
+const filtersFromTop = $('.filters').offset().top;
 
-$(document).on("scroll", function() {
+$(document).on('scroll', function() {
   const scrollValue = $(this).scrollTop();
 
   //header
 
-  if (scrollValue < WindowHeight / 2) $(".header-button").addClass("on-effect");
-  else $(".header-button").removeClass("on-effect");
+  if (scrollValue < WindowHeight / 2) $('.header-button').addClass('on-effect');
+  else $('.header-button').removeClass('on-effect');
 
-  if (scrollValue < WindowHeight / 2) $(".icons").addClass("on-effect");
-  else $(".icons").removeClass("on-effect");
+  if (scrollValue < WindowHeight / 2) $('.icons').addClass('on-effect');
+  else $('.icons').removeClass('on-effect');
 
-  if (scrollValue < WindowHeight / 2) $(".links").addClass("on-effect");
-  else $(".icons").removeClass("on-effect");
+  if (scrollValue < WindowHeight / 2) $('.links').addClass('on-effect');
+  else $('.icons').removeClass('on-effect');
 
   //aboutMe
 
   if (scrollValue > articleFromTop - WindowHeight)
-    $("article").addClass("on-effect");
-  else $("article").removeClass("on-effect");
+    $('article').addClass('on-effect');
+  else $('article').removeClass('on-effect');
 
   if (scrollValue > photoFromTop - WindowHeight)
-    $(".photo").addClass("on-effect");
-  else $(".photo").removeClass("on-effect");
+    $('.photo').addClass('on-effect');
+  else $('.photo').removeClass('on-effect');
 
   //projects
   if (scrollValue > filtersFromTop - WindowHeight)
-    $(".filters").addClass("on-effect");
-  else $(".filters").removeClass("on-effect");
+    $('.filters').addClass('on-effect');
+  else $('.filters').removeClass('on-effect');
 
   $(function() {
-    const projects = [...document.querySelectorAll(".project")];
+    const projects = [...document.querySelectorAll('.project')];
 
     $.each(projects, function(index, place) {
       const projectPlaceFromTop = $(this).offset().top;
       const projectPlaceHeight = $(this).outerHeight();
 
       if (scrollValue > projectPlaceFromTop - WindowHeight) {
-        $(this).addClass("on-effect");
+        $(this).addClass('on-effect');
       }
     });
   });
--- src/js/icon.js
+++ src/js/icon.js
@@ -1,22 +1,22 @@
-const iconsFilter = [...document.querySelectorAll(".icon-filter")];
-const iconsUsed = [...document.querySelectorAll(".used-icon")];
+const iconsFilter = [...document.querySelectorAll('.icon-filter')];
+const iconsUsed = [...document.querySelectorAll('.used-icon')];
 const icons = iconsFilter.concat(iconsUsed);
 
 // filters
 
-const filters = [...document.querySelectorAll(".icon-filter")];
+const filters = [...document.querySelectorAll('.icon-filter')];
 let activeFilters = [];
-const projects = [...document.querySelectorAll(".project")];
+const projects = [...document.querySelectorAll('.project')];
 
 // filtrowanie projektów
 const checkProjects = () => {
   projects.forEach(project => {
-    let used = project.getAttribute("data-filters");
-    const projectsFilters = used.split(" ");
+    let used = project.getAttribute('data-filters');
+    const projectsFilters = used.split(' ');
 
     // jesli nie ma zadnego filtra wszystkie widoczne
     if (activeFilters.length === 0) {
-      project.classList.remove("off");
+      project.classList.remove('off');
     } else {
       // forEach - czy kazdy activeFilter jest w projectsFilters,
 
@@ -25,9 +25,9 @@ const checkProjects = () => {
         const index = projectsFilters.indexOf(name);
 
         if (index !== -1) {
-          project.classList.remove("off");
+          project.classList.remove('off');
         } else {
-          project.classList.add("off");
+          project.classList.add('off');
           return; // jesli nie ma return nie dziala poprawnie filtrowanie (znowu usuwa "off")
         }
       }
@@ -35,10 +35,9 @@ const checkProjects = () => {
   });
 
   // info - brak wyników po filtrowaniu
-  const projectsOff = [...document.querySelectorAll(".project.off")];
-  if (projectsOff.length === 7)
-    document.querySelector(".allProjectsOff").classList.add("on");
-  else document.querySelector(".allProjectsOff").classList.remove("on");
+  const projectsOff = [...document.querySelectorAll('.project.off')];
+  if (projectsOff.length === 8) document.querySelector('.allProjectsOff').classList.add('on');
+  else document.querySelector('.allProjectsOff').classList.remove('on');
 };
 
 // dodanie filtru do tablicy z aktywnymi filtrami
@@ -54,40 +53,40 @@ const addFilter = name => {
 
 //  nasluchiwanie na filtry
 filters.forEach(icon => {
-  icon.addEventListener("click", () => {
-    icon.classList.toggle("on");
-    const name = icon.getAttribute("data-name");
+  icon.addEventListener('click', () => {
+    icon.classList.toggle('on');
+    const name = icon.getAttribute('data-name');
     addFilter(name);
   });
 });
 
 // przycisk "projekty" i "w gorę"
 
-$(".header-button").on("click", function() {
-  $("body, html").animate(
+$('.header-button').on('click', function() {
+  $('body, html').animate(
     {
-      scrollTop: $(".projects").offset().top
+      scrollTop: $('.projects').offset().top
     },
     1000
   );
 });
 
-const button = document.querySelector(".go-up");
+const button = document.querySelector('.go-up');
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > window.innerHeight / 2) {
-    button.classList.add("on");
+    button.classList.add('on');
   } else {
-    button.classList.remove("on");
+    button.classList.remove('on');
   }
 });
 
-button.addEventListener("click", () => {
-  $("body, html").animate(
+button.addEventListener('click', () => {
+  $('body, html').animate(
     {
-      scrollTop: $("header").offset().top
+      scrollTop: $('header').offset().top
     },
     1000
   );
